a86c157b09bc5defe396e1fe4515e4e46852b3bb
Jefferson <jeffeluiz97@gmail.com>
2022-05-04 20:02:19 -0300

    Inicial changes

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Windows.Forms;
+using Views;
+//using Controllers;
+
+namespace EncryptMe
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+
+      try
+      {
+        Application.EnableVisualStyles();
+        Application.Run(new Login());
+      }
+      catch (Exception err)
+      {
+        MessageBox.Show(err.Message);
+      }
+    }
+  }
+}
